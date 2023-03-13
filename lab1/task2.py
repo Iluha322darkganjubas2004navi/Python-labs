1a4e7e86d573ff7b5938144ae03ce14a75661081
GLORY TO RUSSIA!!! <glory_to_russia@Ubuntu.myguest.virtualbox.org>
2023-03-13 16:13:02 +0300

    Adding sub feature to function

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,5 +1,7 @@
 def calc(num1, num2, operator):
     if(operator == "sum"):
         return [num1 + num2, num1, num2]
+    if(operator == "sub"):
+        return [num1 - num2, num1, num2]
     else:
         return "Error!"
