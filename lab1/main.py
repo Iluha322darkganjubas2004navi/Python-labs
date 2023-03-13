b4fb841940f0500fbe1d59b930dc4d5adc15fbb5
GLORY TO RUSSIA!!! <glory_to_russia@Ubuntu.myguest.virtualbox.org>
2023-03-13 16:17:16 +0300

    Starting to create third task and modifiing main.py

--- lab1/main.py
+++ lab1/main.py
@@ -3,4 +3,5 @@ import task2
 import task3
 
 print(task1.print_hello())
-print(task2.calc(1,2, "sum"))
+print(task2.calc(1, 2, "div"))
+print(task3.list_of_evens([1,2,3,4], []))
---
+++ lab1/task3.py
@@ -0,0 +1,2 @@
+def list_of_evens(my_list, temp_list):
+    return my_list
