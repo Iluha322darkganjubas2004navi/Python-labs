63666e582c6f611b0548d9e3a67ab079296bcab0
GLORY TO RUSSIA!!! <glory_to_russia@Ubuntu.myguest.virtualbox.org>
2023-03-13 16:11:20 +0300

    Start to creating second task and modifiing main.py

--- lab1/main.py
+++ lab1/main.py
@@ -3,3 +3,4 @@ import task2
 import task3
 
 print(task1.print_hello())
+print(task2.calc(1,2, "sum"))
---
+++ lab1/task2.py
@@ -0,0 +1,5 @@
+def calc(num1, num2, operator):
+    if(operator == "sum"):
+        return [num1 + num2, num1, num2]
+    else:
+        return "Error!"
