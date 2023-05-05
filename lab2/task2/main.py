4992bf260c0e66c4e1e528ba45c2184b0142cb32
Sanya_Pudge <glory_to_russia@Ubuntu.myguest.virtualbox.org>
2023-05-05 10:29:51 +0300

    Help_and_input

--- lab2/task2/main.py
+++ lab2/task2/main.py
@@ -1,4 +1,27 @@
-def pars_command():
+Commands ='''
+* help - show all commands
+* add <key> [key, …] – add one or more elements to the container (if the element is already in there then don’t add);
+* remove <key> – delete key from container;
+* find <key> [key, …] – check if the element is presented in the container, print each found or “No such elements” if nothing is;
+* list – print all elements of container;
+* grep <regex> – check the value in the container by regular expression, print each found or “No such elements” if nothing is;
+* save - save container to file
+* switch – switches to another user.
+* exit - leave from program
+'''
+def parse_command():
+    user_input=input('').split(maxsplit=1)
+
+    if len(user_input) == 0:
+        print('Input must be not empty')
+
+    command = user_input[0]
+    arguments = ''
+    if len(user_input)>1:
+        arguments = user_input[1]
+    return command, arguments
+
+
 
 print('Welcome to my own set!!!')
 username=input('Enter username: ')
