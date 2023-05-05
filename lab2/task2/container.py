d5eb7e0b6a1a29b8dddd87ed4326e77016843fbb
Sanya_Pudge <glory_to_russia@Ubuntu.myguest.virtualbox.org>
2023-05-06 02:58:50 +0300

    BIG_Fix_of_load

--- lab2/task2/container.py
+++ lab2/task2/container.py
@@ -1,51 +1,104 @@
-import json
-import os
 import re
+import pickle
+
 
 class Container:
-    _username: str
-    _storage: set[str] = set()
-    _filename: str
+    def __init__(self, username):
+        self.username = username
+        self.container = set()
+        self.filename = f'{username}.dat'
+        self.users = {username: self.container}
 
-    def __init__(self, username: str):
-        self._username = username
-        self._filename = f'./data/{username}.json'
-        self.load()
-
-    def add(self, key):
-        self._storage.add(key)
-
-    def add(self, key):
-        self._storage.add(key)
+    def add(self, *keys):
+        added_keys = self.container.union(keys).difference(self.container)
+        not_added_keys = self.container.intersection(keys)
+        if added_keys:
+            print('Added: ', ', '.join(added_keys))
+        if not_added_keys:
+            print('Already in the container: ', ', '.join(not_added_keys))
+        self.container.update(keys)
 
     def remove(self, key):
-        if key in self._storage:
-            self._storage.remove(key)
+        if key in self.container:
+            self.container.remove(key)
+            print(key, ' removed from the container.')
+        else:
+            print(key, 'is not in the container.')
 
-    def find(self, key) -> bool:
-        return key in self._storage
+    def find(self, *keys):
+        found_keys = self.container.intersection(keys)
+        if found_keys:
+            print('Items found: ', ', '.join(found_keys))
+        else:
+            print('No such elements.')
 
     def list(self):
-        return list(self._storage)
+        if self.container:
+            print('All elements in the container:')
+            for element in self.container:
+                print(element)
+        else:
+            print('Container is empty.')
 
-    def grep(self, regex):
-        return list(filter(lambda key: re.match(regex, key), self._storage))
+    def grep(self, regex, ignor_case=False):
+        if ignor_case:
+            pattern = re.compile(regex, re.IGNORECASE)
+        else:
+            pattern = re.compile(regex)
+
+        matches = [key for key in self.container if re.search(pattern, key)]
+
+        if matches:
+            print('Matches found:')
+            for element in matches:
+                print(element)
+        else:
+            print('No such elements.')
 
     def save(self):
-        os.makedirs(os.path.dirname(self._filename), exist_ok=True)
-        with open(self._filename, "w") as outfile:
-            json.dump(list(self._storage), outfile)
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self.container, file)
+        print('Container save to ', self.filename)
+
     def load(self):
-        if os.path.exists(self._filename):
-            with open(self._filename, 'r') as infile:
-                self._storage = set(json.load(infile))
-    def load_from_file(self):
-        if os.path.exists(self.filename):
-            with open(self._filename, 'r') as file:
-                tmp_container = set(json.load(file))
-                for i in tmp_container:
-                    self._storage.add(i)
-    def switch (self, username :str):
-        self._username = username
-        self._filename = f'./data/{username}.json'
-        self._storage.clear()
+        try:
+            with open(self.filename, 'rb') as file:
+                self.container = pickle.load(file)
+            print(self.username, '\'s container loaded.')
+        except FileNotFoundError:
+            print('No saved container for ', self.username)
+
+    def username_check(self, username):
+        if username in self.users.keys():
+            return True
+        else:
+            return False
+
+    def new_user(self, username):
+        if not self.username_check(username):
+            self.users[username] = set()
+            self.container = self.users[username]
+            self.username = username
+            self.filename = f'{username}.dat'
+            print(f'Container of user {username} has been successfully loaded.')
+
+    def switch(self, username):
+        if not re.findall(r'\b[A-z]\w+\b', username):
+            print('Incorrect name.')
+            return
+
+        if self.username_check(username):
+            self.container = self.users[username]
+            self.username = username
+            self.filename = f'{username}.dat'
+            print(f'Container of user {username} has been successfully loaded.')
+        else:
+            print('This user does not exist. Would you like to create one? (y/n):')
+            while True:
+                answer = input('> ')
+                if answer == 'y' or answer == 'Y':
+                    self.new_user(username)
+                    break
+                elif answer == 'n' or answer == 'N':
+                    print(f'Staying in the  container {self.username}.')
+                    break
--- lab2/task2/main.py
+++ lab2/task2/main.py
@@ -1,85 +1,40 @@
-from container import Container
+import container
+import re
 
-Commands = '''
-* help - show all commands
-* add <key> [key, …] – add one or more elements to the container (if the element is already in there then don’t add);
-* remove <key> – delete key from container;
-* find <key> [key, …] – check if the element is presented in the container, print each found or “No such elements” if nothing is;
-* list – print all elements of container;
-* grep <regex> – check the value in the container by regular expression, print each found or “No such elements” if nothing is;
-* save - save container to file
-* switch – switches to another user.
-* exit - leave from program
-'''
-def parse_command():
-    user_input=input('').split(maxsplit=1)
+while True:
+    username = input("Enter username: ")
+    if not re.findall(r'\b[A-z]\w+\b', username):
+        print('Incorrect name.')
+    else:
+        container = container.Container(username)
+        break
 
-    if len(user_input) == 0:
-        print('Input must be not empty')
-
-    command = user_input[0]
-    arguments = ''
-    if len(user_input)>1:
-        arguments = user_input[1]
-    return command, arguments
-
-def ask_save_container(storage: Container):
-    answer = input('Do you want to save container?(y/n): ')
-    if answer == 'y':
-        storage.save()
-
-def ask_load_container(storage: Container):
-    answer = input('Do you want to load container?(y/n): ')
-    if answer == 'y':
-        storage.load()
-def greeting(username: str):
-    print(f'Hello, {username}!')
-def exec_command(command: str, arguments: str, storage: Container) -> bool:
-    match command:
-        case 'help':
-            print(Commands)
+while True:
+    command = input('> ').split()
+    match command[0]:
         case 'add':
-            arguments_list = arguments.split()
-            for el in arguments_list:
-                storage.add(el)
+            container.add(*command[1:])
         case 'remove':
-            storage.remove(arguments)
-        case 'list':
-            print(storage.list())
+            container.remove(command[1])
         case 'find':
-            if len(arguments) != 0:
-                arguments_list=arguments.split()
-                for el in arguments_list:
-                    found = storage.find(el)
-                    print(f'{el} found' if found else f'{el} not found')
+            container.find(*command[1:])
+        case 'list':
+            container.list()
         case 'grep':
-            res = storage.grep(arguments)
-            if len(res) != 0:
-                print('Found values: ' + str(res))
+            if len(command) == 3 and (command[2].lower() == 'true' or command[2].lower() == 'false'):
+                if command[2].lower() == 'true':
+                    container.grep(command[1], True)
+                else:
+                    container.grep(command[1], False)
+            elif len(command) == 2:
+                container.grep(command[1])
+            else:
+                print('You entered the wrong command.')
         case 'save':
-            storage.save
-            print('Container was saved successefully')
+            container.save()
         case 'load':
-            storage.load_from_file()
-        case 'exit':
-            ask_save_container(storage)
-            return False
+            container.load()
         case 'switch':
-            ask_save_container(storage)
-            storage.switch(arguments)
-            greeting(arguments)
-            ask_load_container(storage)
+            container.switch(command[1])
         case _:
-            print('Such command not found')
-    return True
-
-
-print('Welcome to my own set!')
-username = input('Enter username: ')
-storage = Container(username)
-print(f'Hello, {username}!')
-print('Type command or \'help\' to get info about available commands.')
-is_working = True
-while is_working:
-    command, args = parse_command()
-    is_working = exec_command(command, args, storage)
+            print('Such command not supported')
