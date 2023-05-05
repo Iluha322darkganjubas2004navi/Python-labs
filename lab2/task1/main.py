29518283e070107ab2a43aeac8dccd9bcd098148
Sanya_Pudge <glory_to_russia@Ubuntu.myguest.virtualbox.org>
2023-05-06 01:56:51 +0300

    Fix1Task

--- lab2/task1/main.py
+++ lab2/task1/main.py
@@ -1,9 +1,24 @@
 import task1
 
-text = input('Enter text: ')
-print('Amount of sentences: ' + str(task1.amount_of_sentences(text)))
-print('Amount of non-declarative sentences: ' + str(task1.amount_of_non_declarative_sentences(text)))
-print('Average sentence length: ' + str(task1.average_sentence_lenght(text)))
-print('Average word length: ' + str(task1.average_word_lenght(text)))
-print('Top-K repeated N-grams: ' + str(task1.top_k_repeated_n_grams(text)))
+text = input('Enter the text:\n')
 
+sentences = task1.sentences_split(text)
+sentences_amount = task1.sentences_amount(sentences)
+non_declarative_sentences_amount = task1.non_declarative_sentences_amount(sentences)
+average_sentences_length = task1.average_sentence_length(sentences)
+average_word_length = task1.average_word_length(text)
+k=10
+n=4
+n_grams = task1.top_n_grams(k, n, sentences)
+
+print('Amount of sentences: ', sentences_amount)
+print('Amount of non-declarative sentences: ', non_declarative_sentences_amount)
+print('Average length of the sentence in characters: ', average_sentences_length)
+print('Average length of the word in the text in characters', average_word_length)
+
+if n_grams == {}:
+    print('No {0}-grams found.'.format(n))
+else:
+    print('List of top-{0}, {1}-grams: '.format(k, n))
+    for gram, count in n_grams.items():
+        print('Count: {0:>3} | {1}-gram: {2}'.format(count, n, ' '.join(gram)))
--- lab2/task1/task1.py
+++ lab2/task1/task1.py
@@ -1,52 +1,120 @@
 import re
-from constants import (WORDS, NUMBERS, SENTENCES, NON_DECL_SENTENCES,
-                       ABBREVIATIONS1, ABBREVIATIONS2)
 
 
-def amount_of_sentences(text: str) -> int:
-    text = text.lower()
-    amount = len(re.findall(SENTENCES, text))
+def sentences_split(text):
+    abbreviations = ['mr', 'mrs', 'ms', 'dr', 'prof', 'rev', 'etc', 'vs.', 'pl.', 'jr.']
+    sentence_pattern = r'^.*?[.?!](?= |\w|$|"|\')'
+    sentences = []
 
-    for abbreviation in ABBREVIATIONS1:
-        amount -= text.count(abbreviation)
+    match = re.search(sentence_pattern, text)
+    while match:
+        sentence = match.group()
 
-    for abbreviation in ABBREVIATIONS2:
-        amount -= text.count(abbreviation) * 2
+        sentences.append(sentence)
+        text = re.sub(re.sub(r'\?', '\\' + '?', sentence), '', text, count=1)
 
-    return amount
+        single_quotes_count = len(re.findall(r'\'', sentences[-1]))
+        double_quotes_count = len(re.findall(r'\"', sentences[-1]))
+        open_bracket_count = len(re.findall(r'\(', sentences[-1]))
+        closing_bracket_count = len(re.findall(r'\)', sentences[-1]))
+
+        for word in text:
+            if word == ' ':
+                text = text[1:]
+                continue
+            if word == '\'':
+                if single_quotes_count & 1 == 0:
+                    break
+                else:
+                    sentences[-1] = sentences[-1] + '\''
+                    text = text[1:]
+                    continue
+            if word == '\"':
+                if double_quotes_count & 1 == 0:
+                    break
+                else:
+                    sentences[-1] = sentences[-1] + '\"'
+                    text = text[1:]
+                    continue
+            if word == ')':
+                if open_bracket_count - closing_bracket_count == 0:
+                    break
+                else:
+                    sentences[-1] = sentences[-1] + ')'
+                    closing_bracket_count = closing_bracket_count + 1
+                    text = text[1:]
+                    continue
+            break
+
+        match = re.search(sentence_pattern, text)
+
+    i = 0
+
+    while i < len(sentences) - 1:
+        for abbreviation in abbreviations:
+            if re.search(r'\b' + abbreviation + r'\. *$', sentences[i],
+                         re.IGNORECASE):  # and sentences[i + 1][0].isalpha()
+                sentences[i] = sentences[i] + ' ' + sentences[i + 1]
+                del sentences[i + 1]
+                i = i - 1
+                break
+        i = i + 1
+
+    i = 0
+
+    while i < len(sentences) - 1:
+        if (len(sentences[i]) > 2 and sentences[i][-3].isalpha() == False and sentences[i][-2].isalpha() and
+            sentences[i][-1] == '.') \
+                or (len(sentences[i]) <= 2 and sentences[i][-2].isalpha() and sentences[i][-1] == '.') \
+                or (sentences[i][:-1].isdigit() and sentences[i][-1] == '.'):
+            sentences[i] = sentences[i] + ' ' + sentences[i + 1]
+            del sentences[i + 1]
+            continue
+        i = i + 1
+
+    return sentences
 
 
-def amount_of_non_declarative_sentences(text: str) -> int:
-    return len(re.findall(NON_DECL_SENTENCES, text))
+def sentences_amount(sentences):
+    return len(sentences)
+
+def word_list(text):
+    word_pattern = r'\b(?!\d+\b)[\w\']+\b'
+    words = re.findall(word_pattern, text)
+    return words
 
 
-def average_sentence_lenght(text: str) -> float:
-    nums = re.findall(NUMBERS, text)
-    words = [word for word in re.findall(WORDS, text) if word not in nums]
-    words_len = sum(len(word) for word in words)
+def average_word_length(text):
+    words = word_list(text)
+    average_length = sum(map(len, words)) / len(words)
+    return average_length
 
-    if amount_of_sentences(text) != 0:
-        return round(words_len / amount_of_sentences(text), 2)
-    else:
-        return 0
 
-def average_word_lenght(text: str) -> float:
-    words = re.findall(WORDS, text)
-    words_len_in_characters = sum(len(word) for word in words)
+def average_sentence_length(sentences):
+    average_length = 0
+    for sentence in sentences:
+        words = word_list(sentence)
+        average_length += sum(map(len, words))
+    average_length = average_length / len(sentences)
+    return average_length
 
-    if len(words) != 0:
-        return round(words_len_in_characters / len(words), 2)
-    else:
-        return 0
 
-def top_k_repeated_n_grams(text: str, k=10, n=4):
-    words=re.findall(WORDS, text.lower())
-    dictofNgrams={}
-    for i in range(len(words)- n + 1):
-        n_gram=' '.join([str(word) for word in words[i:i + n]])
-        if n_gram not in dictofNgrams:
-            dictofNgrams[n_gram] = 1
+def n_grams_check(n, sentences):
+    n_grams = []
+    for sentence in sentences:
+        words = re.findall(r'\b[\w\']+\b', sentence)
+        n_grams.extend([tuple(words[i:i + n]) for i in range(len(words) - n + 1)])
+
+    n_gram_freq = {}
+    for gram in n_grams:
+        if str(gram).lower() in n_gram_freq:
+            n_gram_freq[str(gram).lower()] += 1
         else:
-            dictofNgrams[n_gram] += 1
-    sorted_ngrams=sorted(dictofNgrams.items(), key=lambda x:x[1],reverse=True)
-    return sorted_ngrams[0:k]
+            n_gram_freq[str(gram).lower()] = 1
+
+    return n_gram_freq
+
+def top_n_grams(k, n, sentences):
+    n_gram_freq = n_grams_check(n, sentences)
+    n_gram_freq = n_grams_sort(n_gram_freq)
+    return dict(list(n_gram_freq.items())[:k])
